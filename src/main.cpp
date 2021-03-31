b5072d8dec8053b519452aea2968d46a8f986d9c
Darie Roman <darieroman10@gmail.com>
2021-03-31 00:26:59 -0400

    the orientation is fully working

--- src/main.cpp
+++ src/main.cpp
@@ -7,7 +7,6 @@ SF fusion;
 float ax, ay, az, gx, gy, gz, mx, my, mz;
 float pitch, roll, yaw;
 float deltat;
-bool ready = true;
 
 void setup() {
     Serial.begin(9600);
@@ -31,21 +30,21 @@ void setup() {
 
 void loop() {
 
-    ready = true;
 
-    if (IMU.accelerationAvailable()) IMU.readAcceleration(ax, ay, az);
-    else ready = false;
-    if (IMU.gyroscopeAvailable()) IMU.readGyroscope(gx, gy, gz);
-    else ready = false;
-    if (IMU.magneticFieldAvailable()) IMU.readMagneticField(mx, my, mz);
-    else ready = false;
+    if (IMU.accelerationAvailable() && IMU.gyroscopeAvailable() && IMU.magneticFieldAvailable()) {
 
-    if (ready) {
+        IMU.readAcceleration(ax, ay, az);
+        IMU.readGyroscope(gx, gy, gz);
+        IMU.readMagneticField(mx, my, mz);
 
+        gx *= 0.0174533f;
+        gy *= 0.0174533f;
+        gz *= 0.0174533f;
+        
+        
         deltat = fusion.deltatUpdate();
 
         fusion.MadgwickUpdate(gx, gy, gz, ax, ay, az, mx, my, mz, deltat);
-        // fusion.MahonyUpdate(gx, gy, gz, ax, ay, az, mx, my, mz, deltat);
 
         pitch = fusion.getPitch();
         roll = fusion.getRoll();
@@ -56,10 +55,6 @@ void loop() {
         Serial.println(yaw);
 
     }
-    
-
-    delay(10);
-
 
 
 }
