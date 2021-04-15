693a72bb6b2c0750d3a06be63083e0c499e2c06e
Darie Roman <darieroman10@gmail.com>
2021-04-15 12:59:47 -0400

    using Servo library, motion is smoother

---
+++ TurretMicro/include/servo_limits.h
@@ -0,0 +1,4 @@
+#define S1_MIN 40
+#define S1_MAX 150
+#define S2_MIN 1
+#define S2_MAX 180
--- TurretMicro/src/main.cpp
+++ TurretMicro/src/main.cpp
@@ -1,44 +1,45 @@
 #include <Arduino.h>
+#include <Servo.h>
+#include "servo_limits.h"
 
 #define SERVO1_PIN 1
 #define SERVO2_PIN 2
 #define LASER_PIN 3
 
+Servo servo1;
+Servo servo2;
+
 void setup() {
-    pinMode(SERVO1_PIN, OUTPUT);
-    pinMode(SERVO2_PIN, OUTPUT);
+
     pinMode(LASER_PIN, OUTPUT);
 
-    Serial.begin(9600);
+    servo1.attach(SERVO1_PIN);
+    servo2.attach(SERVO2_PIN);
     
     digitalWrite(LASER_PIN, HIGH);
-    // delay(500);
-    // digitalWrite(LASER_PIN, LOW);
-    // delay(500);
-    // digitalWrite(LASER_PIN, HIGH);
-    // delay(500);
-    // digitalWrite(LASER_PIN, LOW);
+
+    Serial.begin(9600);
 
 }
 
 void loop() {
 
-    if (Serial.available()) {
+    if (Serial.available() >= 6) {
 
         int device = Serial.readStringUntil('-').toInt();
-        int value = Serial.readString().toInt();
-        Serial.print(device, value);
+        int value = Serial.readStringUntil('\n').toInt();
+        Serial.print(device); Serial.print("-"); Serial.println(value);
 
         switch (device) {
             case 1:
-                if (value >= 0 && value <= 255) {
-                    analogWrite(SERVO1_PIN, value);
-                }
+                value = max(value, S1_MIN);
+                value = min(value, S1_MAX);
+                servo1.write(value);
                 break;
             case 2:
-                if (value >= 0 && value <= 255) {
-                    analogWrite(SERVO2_PIN, value);
-                }
+                value = max(value, S2_MIN);
+                value = min(value, S2_MAX);
+                servo2.write(value);
                 break;
             case 3:
                 if (value == 0 || value == 1) {
@@ -47,5 +48,7 @@ void loop() {
                 break;
         }
     }
+
+    delay(10);
 
 }
